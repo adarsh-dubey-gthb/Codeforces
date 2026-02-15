ccc2a389cdebf0461765407673e0c267515e8c62
adarsh-dubey-gthb <contact.adarshdubey@gmail.com>
2026-02-15 20:43:21 +0530

    contes

---
+++ A_Sieve_of_Erato_67_henes.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;
+    cin>>t;
+    while (t--)
+    {
+        
+        
+        int n;
+        cin>>n;
+    bool yes = false;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin>>x;
+        if (x==67)
+        {
+            yes = true;
+        }
+        
+        
+
+    }
+    cout<< ( yes ? "YES\n" : "NO\n");
+}
+return 0;
+
+
+    
+}
